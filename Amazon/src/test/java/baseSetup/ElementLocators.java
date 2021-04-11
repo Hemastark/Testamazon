587cc58846b74a0d908a37302ca56ff7ed8167c5
mhemalatha <mhemalatha@ERD-L328.corp.exterro.com>
2021-04-11 18:54:10 +0530

    Eclipse commit

---
+++ Amazon/src/test/java/baseSetup/ElementLocators.java
@@ -0,0 +1,12 @@
+package baseSetup;
+
+public class ElementLocators {
+	
+	public String searchBox = "//input[@id='twotabsearchtextbox']";
+	public String searchIcon = "(//input[@type='submit'])[1]";
+	public String giftCard = "//img[@src='https://m.media-amazon.com/images/I/41MVyyuN0cL._AC_UL320_.jpg']";
+    public String addToCart = "//input[@id='gc-buy-box-atc']";
+    public String cart = "//span[@id='nav-cart-count']";
+    public String deleteItem = "//input[@value='Delete']";
+    
+}
